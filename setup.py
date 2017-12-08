6991babc423bb9153bce1aca2c933efecb21e262
Rasmus Bonnevie <rabo@dtu.dk>
2017-12-08 12:14:06 +0100

    Changed PyPI name to relaxflow.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'relax',
-  packages = ['relax'], # this must be the same as the name above
+  name = 'relaxflow',
+  packages = ['relaxflow'], # this must be the same as the name above
   version = '0.1',
   description = 'A small library implementing the REBAR and RELAX stochastic gradient estimators in Tensorflow.',
   author = 'Rasmus Bonnevie',
   author_email = 'rasmusbonnevie@gmail.com',
   url = 'https://github.com/Bonnevie/rebar', # use the URL to the github repo
   download_url = 'https://github.com/Bonnevie/rebar/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['estimator', 'gradient', 'tensorflow', 'discrete', 'sampling'], # arbitrary keywords
+  keywords = ['rebar','relax','probability','expectation','estimator', 'gradient', 'tensorflow', 'discrete', 'sampling'], # arbitrary keywords
   classifiers = [],
 )
